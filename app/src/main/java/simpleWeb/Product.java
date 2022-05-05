d0072f2244a6c90b31b14e54f500a84646691886
Enes AKÇELİK <1yusufsevinc.com>
2022-05-05 17:27:07 +0300

    Product class added

---
+++ app/src/main/java/simpleWeb/Product.java
@@ -0,0 +1,30 @@
+package simpleWeb;
+
+public class Product {
+    private String name;
+    private int count;
+
+    public Product() {
+    }
+
+    public Product(String name, int count) {
+        this.name = name;
+        this.count = count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
